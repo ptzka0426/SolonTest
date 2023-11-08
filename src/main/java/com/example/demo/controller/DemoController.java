c64aca3b09d009e1bd7a805db6e12cc22096ed84
Lt <longtao0426@163.com>
2023-11-08 18:30:42 +0800

    测试网络连通

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -14,13 +14,17 @@ public class DemoController {
 
     @Mapping("/hello")
     public String hello(@Param(defaultValue = "world") Integer userName) throws Exception {
+
         return String.format("Hello %s!", appService.getApp(userName).getUsername());
+
     }
 
     @Mapping("/hello1")
     public String hello1(@Param(defaultValue = "world") Integer userName) throws Exception {
+
         System.out.println("ping:" + NetUtil.ping("47.242.95.94"));
         return String.format("Hello %s!", "测试成功！");
+
     }
 
 }
